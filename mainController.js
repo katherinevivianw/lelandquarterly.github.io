baecc8aa61a112c213fc0675dfa3b49ec89c60df
amyjchen <amyjchen@stanford.edu>
2016-06-16 18:19:32 -0700

    uhhh

--- mainController.js
+++ mainController.js
@@ -1,1 +1,22 @@
 var LQApp = angular.module('LQApp', ['ngRoute']);
+
+LQApp.controller('MainController', ['$scope', function($scope) {
+   // We defined an object called 'main' with a single property 'title' that is used
+   // by the html view template to get the page's title in the browser tab.
+   $scope.main = {};
+   $scope.main.title = 'Leland Quarterly';
+
+   $scope.controller = 'home';
+}]);
+
+LQApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'components/home/homeTemplate.html',
+        controller: 'HomeController'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
